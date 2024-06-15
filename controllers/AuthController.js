61f608183c4dc92c73cee49f6a389d0529e45dbe
AliMoussa-00 <elmoussaouiali0065@gmail.com>
2024-06-15 15:24:51 +0100

    adding task .4

---
+++ controllers/AuthController.js
@@ -0,0 +1,36 @@
+import sha1 from 'sha1';
+import { v4 as uuidv4 } from 'uuid';
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
+
+class AuthController {
+  static async getConnect(req, res) {
+    const { authorization } = req.headers;
+    if (authorization) {
+      const base64Credentials = authorization.split(' ')[1];
+      const credentials = Buffer.from(base64Credentials, 'base64').toString('utf-8');
+      const [email, pwd] = credentials.split(':');
+
+      const userCollection = dbClient.db.collection('users');
+      const user = await userCollection.findOne({ email, password: sha1(pwd) });
+      if (!user) {
+        res.status(401).json({ error: 'Unauthorized' });
+      } else {
+        const uuid = uuidv4();
+        const key = `auth_${uuid}`;
+        await redisClient.set(key, user._id, 24 * 3600);
+
+        res.status(200).json({ token: uuid });
+      }
+    }
+    else {
+      res.status(500).json({ error: 'no authorization header' });
+    }
+  }
+
+  static getDisconnect(req, res) {}
+
+  static getMe(req, res) {}
+}
+
+export default AuthController;
